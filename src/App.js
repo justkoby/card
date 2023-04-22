f82902189c03be9f7fee27c142c1818386e5a910
Solomon Darko <kobbydarko2016@gmail.com>
2023-04-22 02:33:46 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import './styles.css'
+
+function App() {
+  return (
+    <>
+      <div className={'desktop-desktop-wrapper'}>
+      <div className={'desktop-desktop'}>
+      <div className={'desktop-overlap-group'}>
+      <img src='https://firebasestorage.googleapis.com/v0/b/lomokie-d5763.appspot.com/o/images%2F88D56A26-D639-4246-AF8F-67F952EB06A1_1_105_c.jpeg-8b972647-4ae7-47aa-8405-90f2027a761b?alt=media&token=28dde977-1367-4e0a-9573-7a8df8744ec3' className="image" alt=''></img>
+      <div className={'desktop-text-wrapper'}>Preview</div>
+      <div className={'desktop-overlap'}>
+      <p className={'info'}>This is the perfect dress for your dinner <br /> with bae or the girls ,or even your birthday</p>
+      <h1 className='header'>
+        Perfect <br />
+        Date Dress
+      </h1>
+      </div>
+      
+      <div className={'price'}> Ghc 190.00</div>
+      <button className='button'>Add to Cart</button> 
+      </div>
+
+      </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
